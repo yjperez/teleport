b1094706e5f2e01ae9e37d0e15a7d0736e3ce4bb
dmitri <deemok@gmail.com>
2020-12-16 21:14:52 +0100

    Add implicit migrations for legacy web session key path for queries.

--- constants.go
+++ constants.go
@@ -141,8 +141,6 @@ const (
 	ComponentWebProxy = "web"
 
 	// ComponentDiagnostic is a diagnostic service
-
-	// ComponentDiagnostic is a diagnostic service
 	ComponentDiagnostic = "diag"
 
 	// ComponentClient is a client
